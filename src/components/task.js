033f88475e84d15fba82ac992f79a1d208b7381b
Sebastian Springer <info@sebastian-springer.com>
2018-02-11 08:19:46 +0100

    format task times

--- src/components/task.js
+++ src/components/task.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import * as moment from 'moment';
+import * as momentDurationFormat from 'moment-duration-format';
 import { task as taskPropType } from '../types/task';
+
+momentDurationFormat(moment);
 
 export const Task = ({ task }) => {
   return (
     <div>
       <div>{task.name}</div>
-      <div>{task.duration}</div>
-      <div>{task.break}</div>
+      <div>{moment.duration(task.duration, 'seconds').format()}</div>
+      <div>{moment.duration(task.break, 'seconds').format()}</div>
       <div>{task.amount}</div>
     </div>
   );
