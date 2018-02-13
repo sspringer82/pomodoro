e17cb799bbef88cd23742c471ac6d8f9f42e8d0f
Sebastian Springer <info@sebastian-springer.com>
2018-02-13 22:12:43 +0100

    Task styling

--- src/components/task.js
+++ src/components/task.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { formatSeconds } from '../util/formatSeconds';
 import { task as taskPropType } from '../types/task';
 
+const Container = styled.div`
+  display: flex;
+  border-bottom: 1px solid black;
+`;
+
+const Cell = styled.div`
+  margin: 15px;
+  width: 70px;
+`;
+
+const Name = Cell.extend`
+  margin: 15px;
+  width: 200px;
+`;
+
 export const Task = ({ task }) => {
   return (
-    <div>
-      <div>{task.name}</div>
-      <div>{formatSeconds(task.duration)}</div>
-      <div>{formatSeconds(task.break)}</div>
-      <div>{task.amount}</div>
-    </div>
+    <Container>
+      <Name>{task.name}</Name>
+      <Cell>{formatSeconds(task.duration)}</Cell>
+      <Cell>{formatSeconds(task.break)}</Cell>
+      <Cell>{task.amount}</Cell>
+    </Container>
   );
 };
 
