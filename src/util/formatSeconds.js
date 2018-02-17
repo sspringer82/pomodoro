0b464cad6f808a15d8a79c135b2e84b103c8d1ae
Sebastian Springer <info@sebastian-springer.com>
2018-02-17 19:12:05 +0100

    fix seconds formatting

--- src/util/formatSeconds.js
+++ src/util/formatSeconds.js
@@ -4,4 +4,4 @@ import * as momentDurationFormat from 'moment-duration-format';
 momentDurationFormat(moment);
 
 export const formatSeconds = amount =>
-  moment.duration(amount, 'seconds').format();
+  amount === 0 ? '00:00' : moment.duration(amount, 'seconds').format();
