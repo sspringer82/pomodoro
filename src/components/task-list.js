08425358a4165c973f16e52d9678bca358152a51
Sebastian Springer <info@sebastian-springer.com>
2018-02-17 09:36:57 +0100

    add button

--- src/components/task-list.js
+++ src/components/task-list.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { task as taskPropType } from '../types/task';
+import { styleVariables } from '../styles/variables';
 import { Task } from './task';
+import add from '../assets/add.svg';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const AddButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+  width: 70px;
+  border: 1px solid ${styleVariables.secondary};
+  background-color: ${styleVariables.background};
+  margin-top: 4px;
+`;
 
 export const TaskList = ({ tasks, onDelete, onActivate }) => {
   return (
-    <div>
+    <Container>
       {tasks.map((task, index) => (
         <Task
           key={index}
@@ -14,7 +34,10 @@ export const TaskList = ({ tasks, onDelete, onActivate }) => {
           onActivate={onActivate}
         />
       ))}
-    </div>
+      <AddButton>
+        <img src={add} alt="add" />
+      </AddButton>
+    </Container>
   );
 };
 
