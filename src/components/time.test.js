4eee454fcc501b67d1b753886a3966ba91388f62
Sebastian Springer <info@sebastian-springer.com>
2018-03-08 21:05:10 +0100

    time component test

---
+++ src/components/time.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Time } from './time';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const task = {
+  id: 1,
+  name: 'task 1',
+  time: 60,
+  duration: 15 * 60,
+  break: 5 * 60,
+  amount: 0,
+  state: 0,
+  active: false,
+};
+
+describe('<Time />', () => {
+  test('show 1:00 if 60 seconds is passed as prop', () => {
+    const wrapper = shallow(<Time task={task} />);
+    expect(wrapper.find('div > div').text()).toBe('1:00');
+  });
+});
