a8172ffa03472b462aac757fa7d4f276e56fbd6b
Sebastian Springer <info@sebastian-springer.com>
2018-03-17 09:23:04 +0100

    Fix exception when no data is present

--- src/components/time.js
+++ src/components/time.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { formatSeconds } from '../util/formatSeconds';
 import { task as taskPropType } from '../types/task';
 
-export const Time = ({ task: { time } }) => {
+export const Time = ({ task: { time } = {} }) => {
   return (
     <div>
       <div>{formatSeconds(time)}</div>
